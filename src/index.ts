058d7de64660e066088fd8b7c6e76f1bcd5444fd
askermarcus <marcus.asker@pinmeto.com>
2025-09-17 15:38:08 +0200

    add tool for single storeid

--- src/index.ts
+++ src/index.ts
@@ -3,115 +3,162 @@ import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 import { z } from "zod";
 import dotenv from "dotenv";
 import {
-    makePmtRequest,
-    makePaginatedPmtRequest,
-    formatListResponse,
+  makePmtRequest,
+  makePaginatedPmtRequest,
+  formatListResponse,
 } from "./helpers";
 
 dotenv.config({ path: ".env" });
 
 const server = new McpServer({
-    name: "PinMeTo-MCP",
-    version: "1.0.0",
-    capabilities: {
-        resources: {},
-        tools: {},
-    },
+  name: "PinMeTo-MCP",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
 });
 
 server.tool(
-    "get_location",
-    "Get location details for a store from PinMeTo API",
-    {
-        storeId: z.string().describe("The store ID to look up"),
-    },
-    async ({ storeId }) => {
-        if (!process.env.PINMETO_API_URL || !process.env.PINMETO_ACCOUNT_ID) {
-            return {
-                content: [
-                    {
-                        type: "text",
-                        text: "Missing PINMETO_API_URL or PINMETO_ACCOUNT_ID environment variable.",
-                    },
-                ],
-            };
-        }
-        const apiUrl = process.env.PINMETO_API_URL;
-        const accountId = process.env.PINMETO_ACCOUNT_ID;
-
-        const locationUrl = `${apiUrl}/listings/v3/${accountId}/locations/${storeId}`;
-        const locationData = await makePmtRequest(locationUrl);
-
-        if (!locationData) {
-            return {
-                content: [
-                    {
-                        type: "text",
-                        text: "Unable to fetch location data.",
-                    },
-                ],
-            };
-        }
-
-        return {
-            content: [
-                {
-                    type: "text",
-                    text: JSON.stringify(locationData),
-                },
-            ],
-        };
+  "get_location",
+  "Get location details for a store from PinMeTo API",
+  {
+    storeId: z.string().describe("The store ID to look up"),
+  },
+  async ({ storeId }) => {
+    if (!process.env.PINMETO_API_URL || !process.env.PINMETO_ACCOUNT_ID) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Missing PINMETO_API_URL or PINMETO_ACCOUNT_ID environment variable.",
+          },
+        ],
+      };
     }
+    const apiUrl = process.env.PINMETO_API_URL;
+    const accountId = process.env.PINMETO_ACCOUNT_ID;
+
+    const locationUrl = `${apiUrl}/listings/v3/${accountId}/locations/${storeId}`;
+    const locationData = await makePmtRequest(locationUrl);
+
+    if (!locationData) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Unable to fetch location data.",
+          },
+        ],
+      };
+    }
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(locationData),
+        },
+      ],
+    };
+  }
 );
 
 server.tool(
-    "get_locations",
-    "Get all location details for the site from PinMeTo API. Use this to find store ids for locations.",
-    {},
-    async () => {
-        if (!process.env.PINMETO_API_URL || !process.env.PINMETO_ACCOUNT_ID) {
-            return {
-                content: [
-                    {
-                        type: "text",
-                        text: "Missing PINMETO_API_URL or PINMETO_ACCOUNT_ID environment variable.",
-                    },
-                ],
-            };
-        }
-        const apiUrl = process.env.PINMETO_API_URL;
-        const accountId = process.env.PINMETO_ACCOUNT_ID;
-        const url = `${apiUrl}/listings/v3/${accountId}/locations?pagesize=100`;
-        const [data, areAllPagesFetched] = await makePaginatedPmtRequest(url);
-        if (!data || data.length === 0) {
-            return {
-                content: [
-                    {
-                        type: "text",
-                        text: "Unable to fetch location data.",
-                    },
-                ],
-            };
-        }
-        return {
-            content: [
-                {
-                    type: "text",
-                    text: formatListResponse(data, areAllPagesFetched),
-                },
-            ],
-        };
+  "get_locations",
+  "Get all location details for the site from PinMeTo API. Use this to find store ids for locations.",
+  {},
+  async () => {
+    if (!process.env.PINMETO_API_URL || !process.env.PINMETO_ACCOUNT_ID) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Missing PINMETO_API_URL or PINMETO_ACCOUNT_ID environment variable.",
+          },
+        ],
+      };
     }
+    const apiUrl = process.env.PINMETO_API_URL;
+    const accountId = process.env.PINMETO_ACCOUNT_ID;
+    const url = `${apiUrl}/listings/v3/${accountId}/locations?pagesize=100`;
+    const [data, areAllPagesFetched] = await makePaginatedPmtRequest(url);
+    if (!data || data.length === 0) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Unable to fetch location data.",
+          },
+        ],
+      };
+    }
+    return {
+      content: [
+        {
+          type: "text",
+          text: formatListResponse(data, areAllPagesFetched),
+        },
+      ],
+    };
+  }
+);
+
+server.tool(
+  "get_google_location_insights",
+  "Fetch Google metrics for a single location belonging to a specific account.",
+  {
+    storeId: z.string().describe("The store ID to look up"),
+    from: z.string().describe("	The start date format YYYY-MM-DD"),
+    to: z.string().describe("	The end date format YYYY-MM-DD"),
+  },
+  async ({ storeId, from, to }) => {
+    if (!process.env.PINMETO_API_URL || !process.env.PINMETO_ACCOUNT_ID) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Missing PINMETO_API_URL or PINMETO_ACCOUNT_ID environment variable.",
+          },
+        ],
+      };
+    }
+    const apiUrl = process.env.PINMETO_API_URL;
+    const accountId = process.env.PINMETO_ACCOUNT_ID;
+
+    const locationUrl = `${apiUrl}/listings/v4/${accountId}/locations/${storeId}/insights/google?from=${from}&to=${to}`;
+    const locationData = await makePmtRequest(locationUrl);
+
+    if (!locationData) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Unable to fetch location data.",
+          },
+        ],
+      };
+    }
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(locationData),
+        },
+      ],
+    };
+  }
 );
 
 async function main() {
-    dotenv.config({ path: ".env" });
-    const transport = new StdioServerTransport();
-    await server.connect(transport);
-    console.error("PinMeTo MCP running on stdio");
+  dotenv.config({ path: ".env" });
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("PinMeTo MCP running on stdio");
 }
 
 main().catch((error) => {
-    console.error("Fatal error in main():", error);
-    process.exit(1);
+  console.error("Fatal error in main():", error);
+  process.exit(1);
 });
