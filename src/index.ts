2473a2496ac66b7b702c897fc9d051b3e745f2b3
Haohan Yang <haohan.yang@pinmeto.com>
2025-09-22 14:22:53 +0200

    Remove remote server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { createMcpServer } from "./mcp_server.js";
-import { RemoteMcpServer } from "./remote_server.js";
 
 let isRemote = false;
 // Get args from command line
@@ -10,32 +9,14 @@ if (args.length > 2 && args[2] === "remote") {
 }
 
 const server = createMcpServer();
-if (isRemote) {
-  const port = 3000;
-  const remoteServer = new RemoteMcpServer(server);
-  remoteServer.start(port, (error) => {
-    if (error) {
-      console.error("Failed to start server:", error);
-      process.exit(1);
-    }
 
-    // Handle server shutdown
-    process.on("SIGINT", async () => {
-      console.log("Shutting down PinMeTo MCP Remote Server...");
-      process.exit(0);
-    });
-    console.log(`PinMeTo MCP Remote Server listening on port ${port}`);
+const transport = new StdioServerTransport();
+server
+  .connect(transport)
+  .then(() => {
+    console.error("PinMeTo MCP running on stdio");
+  })
+  .catch((error) => {
+    console.error("Fatal error in main():", error);
+    process.exit(1);
   });
-} else {
-  // STDIO
-  const transport = new StdioServerTransport();
-  server
-    .connect(transport)
-    .then(() => {
-      console.error("PinMeTo MCP running on stdio");
-    })
-    .catch((error) => {
-      console.error("Fatal error in main():", error);
-      process.exit(1);
-    });
-}
